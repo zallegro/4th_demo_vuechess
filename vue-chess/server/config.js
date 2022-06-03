d2964bf2688b7612c3de57f2086e6fb24355feea
zallegro <105448022+zallegro@users.noreply.github.com>
2022-06-03 23:27:50 +0200

    WEBSITES_PORT

--- vue-chess/server/config.js
+++ vue-chess/server/config.js
@@ -9,9 +9,9 @@ var config = {
         },
         uri: process.env.MONGO_URL || 'mongodb://mongo:27017/vuegustchess'
     },
-    porthttp: process.env.PORT || 3000,
+    porthttp: process.env.WEBSITES_PORT || 3000,
     multicore: false,
-    https: false,
+    https: true,
     debug: false
 }
 module.exports = config;
