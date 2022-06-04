1f80c825f268dd965801d6760b54b3b4886e97f2
zallegro <105448022+zallegro@users.noreply.github.com>
2022-06-04 20:56:44 +0200

    db off

--- vue-chess/server/config.js
+++ vue-chess/server/config.js
@@ -1,14 +1,14 @@
 var config = {
-    db: {
-        options: {
-            db: {native_parser: true},
-            server: {poolSize: 5},
+//a    db: {
+//a        options: {
+//a            db: {native_parser: true},
+//a            server: {poolSize: 5},
 //  replset: { rs_name: 'myReplicaSetName' },
 //  user: 'admin',
 //  pass: '123456'
-        },
-        uri: process.env.MONGO_URL || 'mongodb://mongo:27017/vuegustchess'
-    },
+//a        },
+//a        uri: process.env.MONGO_URL || 'mongodb://mongo:27017/vuegustchess'
+//a    },
     porthttp: process.env.WEBSITES_PORT || 3311,
     multicore: false,
     https: false,
